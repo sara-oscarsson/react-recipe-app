863ccba7dd12d8bf96aac7ad1c84db1c61e6da78
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-07-13 09:01:41 +0200

    recipes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Recipe from './components/recipe';
 
 function App() {
   const APP_ID = '';
   const APP_KEY = '';
   
-
+  const[recipes, setRecipes] = useState([])
  
   useEffect( () =>{
     getRecipes()
   },[])
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=salad&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data)
+    console.log(data.hits)
+    setRecipes(data.hits)
   }
 
   return (
@@ -23,6 +25,9 @@ function App() {
         <input type="text"/>
         <button type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+      ))}
     </div>
   );
 }
